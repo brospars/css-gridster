734ebb12842b1603e17caab23148cf7c31457c1e
Benoit Rospars <b.rospars@impactiv.fr>
2015-10-15 17:00:40 +0200

    mobile grid implementation & demo page

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
 var gulp      = require('gulp'),
     rename    = require('gulp-rename'),     // Renommage des fichiers
-    sass      = require('gulp-sass'),       // Conversion des SCSS en CSS
-    plumber   = require('gulp-plumber'); // Evite l'arret de watch en cas d'erreur
+    minifyCss = require('gulp-minify-css'), // Minify
+    sass      = require('gulp-sass');       // Conversion des SCSS en CSS
 
 
 // Sass → CSS
 gulp.task('css', function() {
-    return gulp.src('./src/*.scss')    // Prend en entrée les fichiers *.css
-        .pipe(plumber())
-        .pipe(sass())                   // transform le sass en css
+    return gulp.src('./src/*.scss')    // Prend en entrée les fichiers *.scss
+        .pipe(sass())                 // transform le sass en css
         .pipe(gulp.dest('./dist/'));  // Sauvegarde le tout dans /src/style
+});
+
+// CSS → Min CSS
+gulp.task('minify-css', function() {
+  return gulp.src('./dist/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', function() {
     gulp.watch('./src/*.scss', ['css']);
+    gulp.watch('./dist/*.css', ['minify-css']);
 });
 
 // Default Task
-gulp.task('default', ['css', 'watch']);
+gulp.task('default', ['css', 'minify-css', 'watch']);
